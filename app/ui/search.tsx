8d38909f0f350c970ce3f5442265130e9ef5a0cc
Tamás Kovács <kovacs.tamas0408@gmail.com>
2025-10-27 15:03:07 +0100

    Set page parameter to 1 in search functionality

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -11,6 +11,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
 
   const handleSearch = useDebouncedCallback((term: string) => {
     const params = new URLSearchParams(searchParams);
+    params.set('page', '1');
     term ? params.set('query', term) : params.delete('query')
 
     replace(`${pathname}?${params.toString()}`)
